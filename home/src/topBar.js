4469bd6c639def8dfb0eacbd065a364743057144
TaylorTayTyT <taylornguyen904@gmail.com>
2022-10-04 18:03:13 -0400

    button is where i want it to be (kinda)

--- home/src/topBar.js
+++ home/src/topBar.js
@@ -1,11 +1,26 @@
-import "./topNav.sass"
-import fingerprint from "./images/fingerprint.png"
+import "./topNav.sass";
+import fingerprint from "./images/fingerprint.png";
+import { Button, Container, Grid, ListItem, Paper, styled } from "@mui/material";
+
+
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
+
 function TopBar() {
-    return(
-        <div>
-        <div className = "topNav"></div>
-        <img className="fingerprint" src={fingerprint}></img>
-        </div>
+    return (
+        <Container className="topContainer">
+            <div className="topNav">
+            </div>
+            <img className="fingerprint" src={fingerprint} alt="fingerprint"></img>
+            <div className="topBarButton">
+                <Button sx={{ backgroundColor: '#FFF' }}>hello</Button>
+            </div>
+
+        </Container>
     )
 }
 
