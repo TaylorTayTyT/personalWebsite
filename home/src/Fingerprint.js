0d999f3b7e033cb69b39e73c0d3244721ba0c59d
TaylorTayTyT <taylornguyen904@gmail.com>
2022-10-25 18:22:21 -0400

    safety

--- home/src/Fingerprint.js
+++ home/src/Fingerprint.js
@@ -1,8 +1,16 @@
 import fingerprint from "./images/fingerprint.png";
 import "./topNav.sass"
 function Fingerprint() {
-    return(
-        <img className="fingerprint" src={fingerprint} alt="fingerprint"></img>
+    return (
+        <div class = "position">
+            <img className="fingerprint" src={fingerprint} alt="fingerprint"></img>
+            <img class="backgroundFingerprint a" src={fingerprint} alt="fingerprint"></img>
+            <img class="backgroundFingerprint b" src={fingerprint} alt="fingerprint"></img>
+            <img class="backgroundFingerprint c" src={fingerprint} alt="fingerprint"></img>
+            <img class="backgroundFingerprint d" src={fingerprint} alt="fingerprint"></img>
+            <img class="backgroundFingerprint e" src={fingerprint} alt="fingerprint"></img>
+            <img class="backgroundFingerprint f" src={fingerprint} alt="fingerprint"></img>
+        </div>
     )
 }
 
