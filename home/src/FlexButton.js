72a73900f3a09a4cb27ab962d4c98316bb53e6d8
TaylorTayTyT <taylornguyen904@gmail.com>
2022-11-01 17:50:58 -0400

    i can press button and i need to make arrow absolute

--- home/src/FlexButton.js
+++ home/src/FlexButton.js
@@ -1,28 +1,41 @@
-import {Button} from "@mui/material"
+import { Button } from "@mui/material"
 import Flex from "./Flex";
 import "./Flex.sass";
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 
 function FlexButton() {
+    const languages = ["Python", "Java", "C#", "C++", "C"];
+    const [index, setIndex] = useState(0);
+    let language = languages[index];
     const handleClick = () => {
-        Flex(); 
+        Flex();
         setClicked(true);
     };
+    let handleSwipe = (() => {
+        console.log("clicked");
+        let newInd = (index + 1) % 5;
+        setIndex(newInd);
+    });
 
     const [clicked, setClicked] = useState(false);
-    if(!clicked) {
-    return(
-        <div>
-            <br></br><br></br><br></br><br></br>
-        <button onClick={handleClick} className = "button-45">Languages</button>
-        </div>
-    )}
+    if (!clicked) {
+        return (
+            <div>
+                <br></br><br></br><br></br><br></br>
+                <button onClick={handleClick} className="button-45">Languages</button>
+            </div>
+        )
+    }
     else {
-        return(
-            <div class = "res"> 
-                <h1 class = "flex introRes">Comfortable with: </h1>
-                <br/><br/>
-                <h1 class = "flex language">Javascript</h1>
+        return (
+            <div class="res">
+                <h1 class="flex introRes">Comfortable with: </h1>
+                <br /><br />
+                <div class="flex language">
+                    <h1>{language}<button onClick={handleSwipe} class="material-symbols-outlined" style={{ scale: "2", verticalAlign: "middle", paddingLeft: "10px" }}>
+                        chevron_right
+                    </button></h1>
+                </div>
             </div>
         )
     }
