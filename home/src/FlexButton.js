d6904a967b808f65cf53853d4bfc6919fac00a7d
TaylorTayTyT <taylornguyen904@gmail.com>
2022-11-08 09:50:11 -0500

    second page

--- home/src/FlexButton.js
+++ home/src/FlexButton.js
@@ -1,40 +1,32 @@
 import "./Flex.sass";
 import React, { useEffect, useState } from "react";
+import dino from "./images/dinosaur.png";
+import circusBall from "./images/circusBall.png";
 
 function FlexButton() {
-    const [index, setIndex] = useState(0); 
+    const [index, setIndex] = useState(0);
 
     const languages = ["Python", "Java", "C#", "C++", "C"];
     let language = languages[index];
-    const handleClick = () => {
-        setClicked(true);
-    };
 
     let handleSwipe = (() => {
         let newInd = (index + 1) % 5;
         setIndex(newInd);
     });
+    return (
+        <div class="flex language">
+            <img class="dino" src={dino} alt="dino"/>
+            <img class="circusBall" src = {circusBall} alt = "circusBall"/>
+            <div class = "present">
+            <p>Comfortable with: </p>
+            <p id="language" class="animate__animated animate__fadeInDown" style={{ padding: "0", display: "inline" }}>{language}</p>
+            <br/><button onClick={handleSwipe} class="material-symbols-outlined" style={{ scale: "1", verticalAlign: "middle", padding: "0px", display: "inline-block" }}>
+                chevron_right
+            </button>
+            </div>
+        </div>
+    )
 
-    const [clicked, setClicked] = useState(false);
-    if (!clicked) {
-        return (
-            <div>
-                <br></br><br></br><br></br><br></br>
-                <button onClick={handleClick} className="button-45">Languages</button>
-            </div>
-        )
-    }
-    else {
-        return (
-            <div class="flex language ">
-                <p>Comfortable with: </p>
-                <p id="language" class="animate__animated animate__fadeInDown" style={{ padding: "0", display: "inline" }}>{language}</p>
-                <br /><button onClick={handleSwipe} class="material-symbols-outlined" style={{ scale: "1", verticalAlign: "middle", padding: "0px", display: "inline-block" }}>
-                    chevron_right
-                </button>
-            </div>
-        )
-    }
 }
 
 export default FlexButton;
