fb68d95642aaf06e1700ac2b3d1fbc40446b16b4
Taylor Nguyen <taylornguyen904@gmail.com>
2022-11-13 15:23:18 -0500

    Updates

--- src/Github.js
+++
@@ -1,7 +0,0 @@
-function Github() {
-    return(
-        <p style = {{transform: "translateY(1000%)"}}><a href = "https://github.com/TaylorTayTyT">This is my Github</a></p>
-    )
-}
-
-export default Github;
