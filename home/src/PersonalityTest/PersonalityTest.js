3f62458583e9ac6c4afbd4e2aac2e32b6e18d4f0
TaylorTayTyT <taylornguyen904@gmail.com>
2022-11-15 18:55:41 -0500

    title

--- home/src/PersonalityTest/PersonalityTest.js
+++ home/src/PersonalityTest/PersonalityTest.js
@@ -1,18 +1,22 @@
-import { TextField, Container, Box } from "@mui/material";
+import { TextField, Box } from "@mui/material";
+import "./PersonalityTest.sass";
 
 function PersonalityTest() {
-    console.log("hi")
     return (
-        <Box sx={{
-            width: '100%',
-            height: 300,
-            backgroundColor: '#bee1e1', 
-            alignItems: 'center',
-            display: 'flex',
-            justifyContent: 'center'
-          }}>
-            <TextField id="outlined-basic" variant="outlined" label = "Put something here" style = {{verticalAlign: "center"}}/>
-        </Box>
+        <div>
+            <h1 className="title">How similar are you to me?</h1>
+
+            <Box sx={{
+                width: '100%',
+                height: 300,
+                backgroundColor: '#bee1e1',
+                alignItems: 'center',
+                display: 'flex',
+                justifyContent: 'center'
+            }}>
+                <TextField id="outlined-basic" variant="outlined" label="Put something here" style={{ verticalAlign: "center", display: 'block' }} />
+            </Box>
+        </div>
     )
 }
 
