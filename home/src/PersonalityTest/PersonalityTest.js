cef6d895125cfe84a282746951b5338b749b8c9f
Taylor Nguyen <taylornguyen904@gmail.com>
2022-11-26 00:32:45 -0500

    consistency

--- home/src/PersonalityTest/PersonalityTest.js
+++ home/src/PersonalityTest/PersonalityTest.js
@@ -1,13 +1,12 @@
-import { FormGroup, FormControlLabel, FormControl, FormLabel, Box, Checkbox } from "@mui/material";
+import { FormGroup, FormControlLabel, FormControl, Box, Checkbox, Button, FormHelperText } from "@mui/material";
+import { useState } from "react";
 import "./PersonalityTest.sass";
 
 function PersonalityTest() {
+    const [helperText, SetHelperText] = useState('')
+    const [error, SetError] = useState(false)
 
-    const sendData = () => {
-        //this will be used to send an object file of what the score was
-    }
-
-    const error = ['1', '2', '3', '4', '5'].filter((v)=>v).length != 1;
+    const handleSubmit = (event) => { SetHelperText('hello') }
 
     return (
         <div>
@@ -22,51 +21,54 @@ function PersonalityTest() {
                 justifyContent: 'center'
             }}>
                 <div style={{ display: 'block' }}>
-                    <h1 style = {{alignSelf: 'vertical'}}>Question 1: Something</h1>
-                    <FormControl 
-                        component="fieldset"
-                        required
-                        error = {error}
+                    <h1 style={{ alignSelf: 'vertical' }}>Question 1: Something</h1>
+                    <form onSubmit={handleSubmit}>
+                        <FormControl
+                            component="fieldset"
+                            required
+                            error={error}
                         >
-                        <FormGroup aria-label="position" row>
-                            <FormControlLabel
-                                value="top"
-                                control={<Checkbox name = '1'/>}
-                                label="1"
-                                labelPlacement="top"
-                                
-                            />
-                            <FormControlLabel
-                                value="start"
-                                control={<Checkbox name = '2'/>}
-                                label="2"
-                                labelPlacement="top"
-                                
-                            />
-                            <FormControlLabel
-                                value="bottom"
-                                control={<Checkbox name = '3'/>}
-                                label="3"
-                                labelPlacement="top"
-                                
-                            />
-                            <FormControlLabel
-                                value="end"
-                                control={<Checkbox name = '4' />}
-                                label="4"
-                                labelPlacement="top"
-                                
-                            />
-                            <FormControlLabel
-                                value="end"
-                                control={<Checkbox name = '5'/> }
-                                label="5"
-                                labelPlacement="top"
-                                
-                            />
-                        </FormGroup>
-                    </FormControl>
-                    <button style={{ display: 'block' }} onClick={sendData}>Submit</button>
+                            <FormGroup aria-label="position" row>
+                                <FormControlLabel
+                                    value="top"
+                                    control={<Checkbox name='1' />}
+                                    label="1"
+                                    labelPlacement="top"
+
+                                />
+                                <FormControlLabel
+                                    value="start"
+                                    control={<Checkbox name='2' />}
+                                    label="2"
+                                    labelPlacement="top"
+
+                                />
+                                <FormControlLabel
+                                    value="bottom"
+                                    control={<Checkbox name='3' />}
+                                    label="3"
+                                    labelPlacement="top"
+
+                                />
+                                <FormControlLabel
+                                    value="end"
+                                    control={<Checkbox name='4' />}
+                                    label="4"
+                                    labelPlacement="top"
+
+                                />
+                                <FormControlLabel
+                                    value="end"
+                                    control={<Checkbox name='5' />}
+                                    label="5"
+                                    labelPlacement="top"
+
+                                />
+                            </FormGroup>
+                            <FormHelperText>{helperText}</FormHelperText>
+                            <Button type='submit'>Submit</Button>
+                        </FormControl>
+                    </form>
                 </div>
             </Box>
         </div>
